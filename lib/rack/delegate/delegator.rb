1ce6fe07acf2656df0e5264ae64a84d71a7068b5
Seth Webster <sethwebster@gmail.com>
2019-01-18 15:05:43 -0500

    More work on multiple upstreams

--- lib/rack/delegate/delegator.rb
+++ lib/rack/delegate/delegator.rb
@@ -5,25 +5,36 @@ module Rack
     class Delegator
       def initialize(urls, uri_rewriters, net_http_request_rewriter, timeout_response)
         @urls = if urls.is_a? Array
-                  urls.map { |u| URI(u) }
+                  urls.map { |u| map_to(u) }
                 else
                   [URI(urls)]
                 end
-        puts @urls
+        # puts @urls.to_s
+        # puts "--- #{@urls.length}"
         @uri_rewriters = uri_rewriters
         @net_http_request_rewriter = net_http_request_rewriter
         @timeout_response = timeout_response
       end
 
       def call(env)
-        if @urls.length > 1
-          call_multi(env)
-        else
-          call_single(env, @urls[0])
-        end
+        res = nil
+        res = if @urls.length > 1
+                call_multi(env)
+              else
+                call_single(env, @urls[0])[:response]
+              end
+        res
       end
 
       private
+
+      def map_to(to)
+        if to.is_a? Hash
+          { uri: URI(to[:uri]), domain: to[:domain] }
+        elsif to.is_a? String
+          { uri: URI(to), domain: nil }
+        end
+      end
 
       def call_single(env, url)
         rack_request = Request.new(env)
@@ -32,11 +43,11 @@ module Rack
                                                      @net_http_request_rewriter)
                                                 .build
 
-        http_response = Net::HTTP.start(*net_http_options(url)) do |http|
+        http_response = Net::HTTP.start(*net_http_options(url[:uri])) do |http|
           http.request(net_http_request)
         end
 
-        convert_to_rack_response(http_response)
+        { response: convert_to_rack_response(http_response), url: url }
       rescue TimeoutErrors
         @timeout_response.call(env)
       end
@@ -55,16 +66,18 @@ module Rack
           response = {
             responses: results.map do |r|
               {
-                status_code: status_of(r),
-                body: body_of(r)
+                status_code: status_of(r[:response]),
+                headers: headers_of(r[:response]),
+                domain: r[:url][:domain],
+                body: body_of(r[:response])
               }
             end
           }
-          return [safe_max_status(results),
-                  headers_of(results[0]),
+          return [safe_max_status(results.map { |r| r[:response] }),
+                  headers_of(results[0][:response]),
                   [response.to_json]]
         end
-        [status_of(results[0]), headers_of(results[0]), body_of(results[0])]
+        [status_of(results[0][:response]), headers_of(results[0][:response]), body_of(results[0][:response])]
       end
 
       def status_of(response)
@@ -80,19 +93,18 @@ module Rack
       end
 
       def safe_parse(str)
-        return str if str.length == 0
+        return str if str.empty?
+
         res = JSON.parse(str)
-        binding.pry
         res
       rescue StandardError
-        binding.pry
         str
       end
 
       def safe_max_status(responses)
         res = responses.max_by { |a| a[0].to_i }[0]
-        if (res == 304)
-          res = responses.all? {|r| r[0].to_i == 304} ? 304 : 200
+        if res == 304
+          res = responses.all? { |r| r[0].to_i == 304 } ? 304 : 200
         end
         res
       end
